5176a3a9390bfac4c5b1305dada5fde49c3d127d
sato310-sato <under_color27@yahoo.co.jp>
2020-09-14 19:57:39 +0900

    AjaxAppの準備

--- db/migrate/20200830033003_create_posts.rb
+++ db/migrate/20200830033003_create_posts.rb
@@ -2,6 +2,8 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.text :content
+      t.boolean :checked 
+      # 既読か未読かを判断
       t.timestamps
     end
   end
